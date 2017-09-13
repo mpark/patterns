3007ee6ee2d67ed8ecb01e7f8d88d0a2ae7e918f
Michael Park <mcypark@gmail.com>
2017-09-13 07:56:56 -0700

    Fix exception support detection on MSVC.

--- include/mpark/patterns/config.hpp
+++ include/mpark/patterns/config.hpp
@@ -12,7 +12,8 @@
 #define __has_feature(x) 0
 #endif
 
-#if __has_feature(cxx_exceptions) || defined(__cpp_exceptions)
+#if __has_feature(cxx_exceptions) || defined(__cpp_exceptions) || \
+    (defined(_MSC_VER) && defined(_CPPUNWIND))
 #define MPARK_PATTERNS_EXCEPTIONS
 #endif
 
